f84554eeae1cb99e439dc53978dd45763bbe9a68
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-03-08 08:40:59 +0000

    Setup and migrate DB on startup

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /skittles-br/tmp/pids/server.pid
 
+# Create the DB and migrate as required
+bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
