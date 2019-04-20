9566f87c2fdf72bd217754ebafec12bf596dd798
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-04-20 12:23:05 +0100

    Initialise CSS support

--- web/app/javascript/packs/app/index.js
+++ web/app/javascript/packs/app/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
-import { API_WS_ROOT } from './constants';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './routes'
+import { API_WS_ROOT } from './constants'
+
+import '../../src/app'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Routes />, document.getElementById('app'),
+    <Routes />, document.getElementById('app')
   )
-});
+})
