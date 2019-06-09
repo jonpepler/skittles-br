1781a9c3665b2ae9f128444d398f7c6c3e8dec54
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-06-10 00:02:04 +0100

    Add scopes to Game with and without players

--- web/app/models/game.rb
+++ web/app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   has_many :players
+
+  def self.with_players
+    includes(:players).where.not(players: { id: nil })
+  end
+
+  def self.without_players
+    includes(:players).where(players: { id: nil })
+  end
 end
