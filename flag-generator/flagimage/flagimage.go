a816c77573952fe8fa3008a830ca873a50bd4ece
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-06-30 12:02:54 +0100

    FG: Increase brightness/value of each colour palette

--- flag-generator/flagimage/flagimage.go
+++ flag-generator/flagimage/flagimage.go
@@ -19,9 +19,15 @@ func New(layers int, responseWriter http.ResponseWriter) Flag {
   f := Flag {layers}
  	responseWriter.Header().Set("Content-Type", "image/svg+xml")
 
-	colour_palette, err := colorful.HappyPalette(6)
+	starter_colour_palette, err := colorful.HappyPalette(6)
 	if err != nil {
 		fmt.Printf("Error generating happy palette: %v", err)
+	}
+
+	var colour_palette [6]colorful.Color
+	for i, v := range starter_colour_palette {
+		h, s, _ := v.Hsv()
+		colour_palette[i] = colorful.Hsv(h, s, 100)
 	}
 
   s := svg.New(responseWriter)
@@ -31,18 +37,20 @@ func New(layers int, responseWriter http.ResponseWriter) Flag {
 
   for i := 0; i < layers; i++ {
   	colour := colour_palette[i + 1]
-  	add_layer(s, 100 * i + 175, 250, 250, 0, colour)
+  	add_random_layer(s, colour)
   }
 
   s.End()
   return f
 }
 
-func add_layer(s *svg.SVG, x int, y int, w int, h int, colour colorful.Color) {
+func add_random_layer(s *svg.SVG, colour colorful.Color) {
 	colour_string := colour.Hex()
+	x, y, w, h := 100 * rand.Intn(5) + 175, 250, 250, 0
+	h++
 	layerFuncs := []func(){
 	    func() { s.Circle(x, y, w/2, fmt.Sprintf("fill:%s;stroke:%s", colour_string, colour_string)) },
-	    func() { s.Circle(x, y, w/2, fmt.Sprintf("fill:none;stroke:%s", colour_string)) },
+	    func() { s.Circle(x, y, w/2, fmt.Sprintf("fill:none;stroke:%s;stroke-width:10;", colour_string)) },
 	}
 	layerFuncs[rand.Intn(len(layerFuncs))]()
 }
