4217beb0e8a45e5ee519103b6a2065b05b3d2563
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-07-02 01:23:51 +0100

    FG: Enable rectangles and anchor points for flag generation

--- flag-generator/flagimage/flagimage.go
+++ flag-generator/flagimage/flagimage.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 	"fmt"
 	"math/rand"
+	"math"
 )
 
 type Flag struct {
-    layers   int
+  layers   int
+}
+
+type coord struct {
+	x int
+	y int
 }
 
 var ratio = 1.5
 var flag_height = 500.0
+var grid_size = 50
+var anchor_points = generate_anchor_points()
 
 func New(layers int, responseWriter http.ResponseWriter) Flag {
   f := Flag {layers}
@@ -46,11 +54,22 @@ func New(layers int, responseWriter http.ResponseWriter) Flag {
 
 func add_random_layer(s *svg.SVG, colour colorful.Color) {
 	colour_string := colour.Hex()
-	x, y, w, h := 100 * rand.Intn(5) + 175, 250, 250, 0
-	h++
 	layerFuncs := []func(){
-	    func() { s.Circle(x, y, w/2, fmt.Sprintf("fill:%s;stroke:%s", colour_string, colour_string)) },
-	    func() { s.Circle(x, y, w/2, fmt.Sprintf("fill:none;stroke:%s;stroke-width:10;", colour_string)) },
+	    func() {
+	    	x, y := rand.Intn(int(get_flag_width())), rand.Intn(int(get_flag_height()))
+	    	r := rand.Intn(int(get_flag_height()) / 2)
+	    	s.Ellipse(x, y, r, r, fmt.Sprintf("fill:%s;stroke:%s", colour_string, colour_string))
+	    },
+	    func() {
+	    	x, y := rand.Intn(int(get_flag_width())), rand.Intn(int(get_flag_height()))
+	    	r := rand.Intn(int(get_flag_height()) / 2)
+	    	s.Ellipse(x, y, r, r, fmt.Sprintf("fill:none;stroke:%s;stroke-width:10;", colour_string))
+	    },
+	    func() {
+	    	a := anchor_points[rand.Intn(len(anchor_points))]
+	    	b := a.distance(anchor_points[rand.Intn(len(anchor_points))])
+	    	s.Rect(a.x, a.y, abs(b.x), abs(b.y), fmt.Sprintf("fill:%s;stroke:%s", colour_string, colour_string))
+	    },
 	}
 	layerFuncs[rand.Intn(len(layerFuncs))]()
 }
@@ -69,4 +88,22 @@ func get_flag_height() float64 {
 
 func get_flag_width() float64 {
 	return flag_height * ratio
+}
+
+func generate_anchor_points() (ap []coord) {
+	for i := 0; i < int(get_flag_height()); i += grid_size {
+		for j := 0; j < int(get_flag_width()); j += grid_size {
+			ap = append(ap, coord{j, i})
+		}
+	}
+	return
+}
+
+func (p1 coord) distance(p2 coord) (vector coord) {
+	vector = coord{p2.x - p1.x, p2.y - p1.y}
+	return
+}
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
 }
