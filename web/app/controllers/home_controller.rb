eec937b71630a8ec5d8c011caa8ed89d98eaddae
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-09-05 00:23:37 +0100

    Web: Show SVG XML text of a random flag on home page load

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def main
     g = current_or_guest_player
     cookies.signed[:player_id] = g.id
+
+    uri = URI("http://flag-generator:3002/random-flag")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.path)
+    response = http.request(request)
+    @flag_image = response.body
   end
 end
