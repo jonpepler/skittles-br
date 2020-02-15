0549ffd182d230a033024f881b4cd5c950a61e3b
Jonathan Pepler <jonathan.pepler@gmail.com>
2020-02-15 19:56:12 +0000

    Add development Bootsnap config to fix byebug behaviour

--- web/config/boot.rb
+++ web/config/boot.rb
@@ -2,3 +2,15 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+if ENV['RAILS_ENV'] == 'development'
+  Bootsnap.setup(
+    cache_dir:            'tmp/cache',
+    development_mode:     true,
+    load_path_cache:      true,
+    autoload_paths_cache: true,
+    disable_trace:        false,
+    compile_cache_iseq:   true,
+    compile_cache_yaml:   true
+  )
+end
