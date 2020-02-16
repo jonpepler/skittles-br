130cd0279f154b45c0185315330ca7529434eff3
Jonathan Pepler <jonathan.pepler@gmail.com>
2020-02-16 23:55:14 +0000

    Remove debug line

--- web/app/jobs/complete_game_job.rb
+++ web/app/jobs/complete_game_job.rb
@@ -2,7 +2,6 @@ class CompleteGameJob < ApplicationJob
   queue_as :default
  
   def perform(game)
-    puts "\n\n!!!!!!!! Running on game #{game.id}\n\n"
     game.update({ status: "complete" })
     game.update_matchmaking_status
   end
