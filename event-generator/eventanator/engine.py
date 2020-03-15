eff85d0e34ed9493d0c9f0e8c207a602be5fa47e
Sam Sheehy <samuelsheehy95@gmail.com>
2020-03-15 21:03:39 +0000

    Implement basic event generator.

---
+++ event-generator/eventanator/engine.py
@@ -0,0 +1,43 @@
+import json
+
+EXAMPLE_EVENT_DICT = {
+    "name": "Random Event Name",
+    "description": "A longer form description (optional)",
+    "requirement": { 
+        "red": 1, "orange": 2, "yellow": 0, "purple": 0, "green": 3 
+    },
+    "reward": { 
+        "red": 0, "orange": 0, "yellow": 3, "purple": 1, "green": 0 
+        },
+    "penalty": { 
+        "red": 1, "orange": 1, "yellow": 1, "purple": 0, "green": 0 
+    }
+}
+
+class EventsEngine:
+    """
+    Principal generator class of events for the Skittles
+    game.
+    """
+    def __init__(self):
+        """
+        Instantiate with the engine's parameters.
+        """
+        pass
+    
+    def generate_random_event(self):
+        """
+        """
+        return Event()
+
+
+class Event:
+    def __init__(self):
+        self.data = EXAMPLE_EVENT_DICT
+
+    @property
+    def json(self):
+        """
+        Get the event in JSON format.
+        """
+        return json.dumps(self.data)
